cb4e1f89ebdaac4eeb0652b32f2144761e5ec1db
moringa <oscarokumu47@gmail.com>
2022-08-16 18:16:54 +0300

    initial commits

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  // let message;
+  if (ride<=400){
+    return('This one is on me!');
+  }else if (ride => 2000  && ride <=2500){
+    return("I will gladly take your thirty bucks.");
+  }else {
+    return 'No can do'
+  }
+  
 }
+console.log(scuberGreetingForFeet(500))
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+if (city="NYC"){
+  return 'Ok,sounds good.';
 }
+else if (city = 'Pittsburgh'){
+  return 'No go.';
+}
+  
+}
+console.log(ternaryCheckCity('Pittsburgh'))
 
 function switchOnCharmFromTip(){
   // Write your code here!
